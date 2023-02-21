5defc3401b3a62fd6ec6411b39a49a62c99d1ce4
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-02-21 22:05:48 +0430

    Update index.js removed unnecessory assets

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import _ from 'lodash';
 import './style.css';
-import Logo from './Capture.PNG';
 function component() {
   const element = document.createElement('div');
-  const image = new Image(200, 150);
-  image.src = Logo;
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(image);
 
   return element;
 }
